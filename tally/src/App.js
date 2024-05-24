405173001cc81f6343616955539c1779cb0dbe72
Gabe Estrada <estradagabe1996@gmail.com>
2024-05-24 10:44:01 -0400

    beginning files

---
+++ tally/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Thenavbar from './Components.js/Navbar';
+
+function App() {
+  return (
+    <>
+      <Thenavbar/>
+    </>
+  );
+}
+
+export default App;
